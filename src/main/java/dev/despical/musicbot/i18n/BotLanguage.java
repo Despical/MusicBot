974ab91d3261f5d8d1b8a1116453a60beb1d2a7a
Berke Akçen <berkeakcen@gmail.com>
2026-06-15 22:39:42 +0300

    Updated license header

--- src/main/java/dev/despical/musicbot/i18n/BotLanguage.java
+++ src/main/java/dev/despical/musicbot/i18n/BotLanguage.java
@@ -1,6 +1,6 @@
 /*
- * MusicBot - A self-hostable Discord music bot with slash-command playback.
- * Copyright (C) 2026 Despical
+ * MusicBot - A modern Discord music bot with polished embeds and playback controls.
+ * Copyright (C) 2026  Berke Akçen
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -9,11 +9,11 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 package dev.despical.musicbot.i18n;
 
